653fd7021eb4867a59134cf8625f2890561e2f69
kszeto1 <kenny.szeto1@gmail.com>
2019-05-01 15:50:07 -0700

    Implement schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,24 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE user (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL
 );
 
-/* Create other tables and define schemas for them here! */
-
-
-
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  text VARCHAR(256) NOT NULL,
+  room VARCHAR(20) NOT NULL,
+  user_id INT NOT NULL, 
+  FOREIGN KEY (user_id)
+    REFERENCES user(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
+      mysql -u root -p < server/schema.sql
  *  to create the database and the tables.*/
 
