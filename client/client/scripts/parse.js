d5e73003f2ab7a5b85dd6ee1d1776047d3d365b8
kszeto1 <kenny.szeto1@gmail.com>
2019-05-02 19:56:35 -0700

    Implement connection for client

---
+++ client/client/scripts/parse.js
@@ -0,0 +1,32 @@
+var Parse = {
+
+  server: `http://127.0.0.1:3000/classes/messages`,
+
+  create: function(message, successCB, errorCB = null) {
+
+    $.ajax({
+      url: `http://127.0.0.1:3000/classes/messages`,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function (error) {
+        console.error('chatterbox: Failed to create message', error);
+      }
+    });
+      },
+
+  readAll: function(successCB, errorCB = null) {
+    $.ajax({
+      url: `http://127.0.0.1:3000/classes/messages`,
+      type: 'GET',
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
+  }
+
+};
